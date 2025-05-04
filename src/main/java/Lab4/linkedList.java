cddad903b1bc58aebcd45ed558cb8b7c8d9eddb1
Andrew Mueller <awm6658@g.rit.edu>
2025-05-04 16:08:52 -0400

    complete

--- src/main/java/Lab4/linkedList.java
+++ src/main/java/Lab4/linkedList.java
@@ -17,6 +17,19 @@ public class linkedList {
         first = newLink;
     }
 
+    public void insertLast(int d){
+        if (isEmpty()){
+            insertFirst(d);
+            return;
+        }
+        link current = first;
+        while(current.next != null){
+            current = current.next;
+        }
+        link newLink = new link(d); 
+        current.next = newLink;
+    }
+
     public link deleteFirst(){
         link temp = first;
         first = first.next;
@@ -34,9 +47,28 @@ public class linkedList {
     }
 
     public linkedList reorder(linkedList sample){
+        linkedList odds = new linkedList();
+        linkedList evens = new linkedList();
+        link current = sample.first;
+        int counter = 1;
 
+        while(current != null){
+            if((counter % 2) == 1){
+                odds.insertLast(current.data);
+            } else {
+                evens.insertLast(current.data);
+            }
+            current = current.next;
+            counter++;
+        }
+        
+        current = odds.first;
+        while(current.next != null){
+            current = current.next;
+        }
+        
+        current.next = evens.first;
 
-
-        return null;
+        return odds;
     }
 }
--- src/main/java/Lab4/run.java
+++ src/main/java/Lab4/run.java
@@ -2,7 +2,21 @@ package main.java.Lab4;
 
 public class run {
     public static void main(String[] args) {
+        linkedList sample = new linkedList();
+        sample.insertFirst(2);
+        sample.insertLast(1);
+        sample.insertLast(3);
+        sample.insertLast(5);
+        sample.insertLast(6);
+        sample.insertLast(4);
+        sample.insertLast(7);
+
+        sample.displayList();
+        System.out.println("reordering...");
+        linkedList x = sample.reorder(sample);
         
+        x.displayList();
+
     } 
 }
 
